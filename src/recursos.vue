5e8fbaeb8925151901b5a53b9c08476a2f13bbf5
Jaasiel <114452071+PoteurDamour@users.noreply.github.com>
2022-10-18 12:56:35 -0500

    VueCARDs - 10/18/2022
    Mini ejemplo de venta de tacos con el uso de puros CARDs

---
+++ src/recursos.vue
@@ -0,0 +1,14 @@
+<template>
+    <img src="https://ecucuba.com/wp-content/uploads/2021/07/birria-tacos-171189-2.jpg" slot="tbirria">
+    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fvivirmejor.mx%2Fcelebraciones-especiales%2Freceta-tacos-al-pastor%2F&psig=AOvVaw0eS46Vv3vf41lmsU5IOx-T&ust=1663951028077000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCKjp1OLqqPoCFQAAAAAdAAAAABAI" slot="tpastor">
+    <img src="https://cdn.colombia.com/gastronomia/2011/08/26/tacos-de-bistec-de-carne-de-res-3177.jpg" slot="tbistec">
+    <img src="https://tvazteca.brightspotcdn.com/dims4/default/4159a1d/2147483647/strip/true/crop/1024x538+0+115/resize/1200x630!/quality/90/?url=https%3A%2F%2Ftvazteca.brightspotcdn.com%2Fc8%2Fb4%2F0b12a838577b8bca600f626dfaa1%2F0" slot="tbarbacoa">
+    <img src="https://img-global.cpcdn.com/recipes/ff1133b6d96b069e/1200x630cq70/photo.jpg" slot="tdorado"> 
+    <img src="https://img-global.cpcdn.com/recipes/91586166b8192e50/1200x630cq70/photo.jpg" slot="tsal">
+</template>
+
+<script>
+    export default{
+        name: 'App'
+    }
+</script>
